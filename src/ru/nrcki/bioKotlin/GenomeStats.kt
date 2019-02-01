39f52cdff9f4c9adbf4d28eb1e1f0554496dfa09
laxeye <oscypek@ya.ru>
2019-02-01 03:58:51 +0300

    FastaRecord integration

--- src/ru/nrcki/bioKotlin/GenomeStats.kt
+++ src/ru/nrcki/bioKotlin/GenomeStats.kt
@@ -2,6 +2,7 @@ package ru.nrcki.bioKotlin
 
 import java.io.*
 import kotlin.math.*
+import ru.nrcki.bioKotlin.*
 /*
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
@@ -12,14 +13,10 @@ data class GenomeData(val fileName: String,
 	val GC: Double,	val n50: Int, val n90: Int, 
 	val totalN: Int, val maxLength: Int)
 
-fun GC(dna: String): Double = 100.0 * dna
-	.map{if ((it == 'C') || (it == 'G')) 1 else 0}.sum()
-
-class Sequence(val id: String, val dna: String){
-	val length = dna.length
-	val gc = GC(dna.toUpperCase()).times(100).div(length).roundToLong().div(100.0)
-	val N = dna.toUpperCase().map{if (it == 'N') 1 else 0}.sum()
-}
+//GC content in percent
+fun getGC(dna: String): Double = dna.toUpperCase()
+	.map{if ((it == 'C') || (it == 'G')) 1 else 0}.sum().times(100.0).div(dna.length)
+fun getNs(dna: String): Int = dna.toUpperCase().map{if (it == 'N') 1 else 0}.sum()
 
 fun mapToJSONString(m: Map<String, Any>): String{
 	val oList = mutableListOf<String>()
@@ -31,70 +28,43 @@ fun mapToJSONString(m: Map<String, Any>): String{
 	return oList.joinToString(separator=", \n", prefix="{", postfix="}")
 }
 
-fun checkID(str: String): Boolean = ((str.first() == '>') && (str.length > 1))
-
 fun main(args: Array<String>){
 	val longMode = false
-	val outMode = "json"
-	val seqList = mutableListOf<Sequence>()
+	val outMode = "text"
 	if (args.size == 0){
 		println("Error: missing filename!")
 		return
 	}
-	val fileName = args[0]
-	val seqFile = File(fileName).bufferedReader().readLines()
-	if(seqFile.size == 0){
-		println("Error: empty file")
-		return
-	}
+	val filename = args[0]
 
-	if(!checkID(seqFile[0])){
-		println("Error: bad sequence file!")
-		return
-	}
-
-	var seq: String
-	var id: String = ""
-
-	var lineArray = mutableListOf<String>()
-	for (line in seqFile){
-		if(line.length > 0){
-			if (line[0] == '>'){
-				seq = lineArray.joinToString(separator="")
-				lineArray = mutableListOf<String>()
-				if (seq.length > 0){
-					seqList.add(Sequence(id, seq))
-				}
-				id = line.drop(1)
-			}else{
-				lineArray.add(line)
-			}
-		}
-	}
-
-	seqList.add(Sequence(id, lineArray.joinToString(separator="")))
+	val seqList = readFasta(filename)
 	
-	val totalLength = seqList.map{it.length}.sum()
-	val totalDNA = seqList.map{it.dna}.joinToString(separator="").toUpperCase()
-	val totalGC = GC(totalDNA).times(100).div(totalLength).roundToLong().div(100.0)
-	val totalN = seqList.map{it.N}.sum()
+	//val totalLength = seqList.map{it.length}.sum()
+	val totalDNA = seqList.map{it.sequence}.joinToString(separator="")
+	val totalLength = totalDNA.length
+	val totalGC = getGC(totalDNA)
+	val totalN = getNs(totalDNA)
 	val contigCount = seqList.size
 	val lengths = seqList.map{it.length}.sortedWith(compareBy({-it}))
 	var sum = 0
 	var n50 = 0
 	var n90 = 0
+	var l50 = 0
+	var l90 = 0
 	lengths.forEach(){
 		sum += it
 		if(n50 == 0 && sum > totalLength/2){
 			n50 = it
+			l50 = lengths.indexOf(it)
 		}
 		if(n90 == 0 && sum*1.0 > totalLength*0.9){
 			n90 = it
+			l90 = lengths.indexOf(it)
 		}
 	}
 
 	if(outMode=="json"){
-		val genomeDataMap = mapOf("Filename" to fileName, "Total_length" to totalLength, 
+		val genomeDataMap = mapOf("Filename" to filename, "Total_length" to totalLength, 
 			"Contig_count" to contigCount,
 			"Total_GC" to totalGC, "N50" to n50, "N90" to n90, "Ambiguous_bases" to totalN, 
 			"Max_length" to lengths[0])
@@ -103,8 +73,9 @@ fun main(args: Array<String>){
 	}else{
 		println("Total length: $totalLength\nContig count: $contigCount")
 		println("Maximum contig length: ${lengths[0]}")
-		println("GC, %: $totalGC\nAmbiguous bases: $totalN")
-		println("N50: $n50\nN90: $n90")			
+		println("GC, %: ${"%.2f".format(totalGC)}\nAmbiguous bases: $totalN")
+		println("N50: $n50\nN90: $n90")
+		println("L50: $l50\nN90: $l90")
 	}
 
 /*	val genomeData = GenomeData(fileName, totalLength, contigCount,
@@ -120,7 +91,7 @@ fun main(args: Array<String>){
 	if(longMode){
 		println("ID\tLength\tGC, %\tN")
 		seqList.forEach(){
-			println("${it.id}\t${it.length}\t${it.gc}\t${it.N}")
+			println("${it.id}\t${it.length}\t${getGC(it.sequence)}\t${getNs(it.sequence)}")
 		}
 	}
 }
