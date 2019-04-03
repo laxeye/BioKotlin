879f48744ab69bbfdcb20d8bc609a1a9efaaf32b
aleksei <oscypek@ya.ru>
2019-04-03 18:39:35 +0300

    U: Non-redundant tetranucleotides added

--- src/ru/nrcki/bioKotlin/tetra.kt
+++ src/ru/nrcki/bioKotlin/tetra.kt
@@ -1,39 +1,81 @@
 package ru.nrcki.bioKotlin
 
 import ru.nrcki.bioKotlin.Fasta
+import ru.nrcki.bioKotlin.revComp
 
-fun makeAllTetra(): List<String>{
+/*Returns all possible or only non self reverse-complemented tetramers*/
+fun makeTetraList(redundant: Boolean = false): List<String>{
 	val dinucs = mutableListOf<String>()
 	val tetranucs = mutableListOf<String>()
 	val nucs = listOf("A","C","G","T")
 	for(n in nucs){
 		nucs.map{dinucs.add(n + it)}
 	}
-	for(n in dinucs){
-		dinucs.map{tetranucs.add(n + it)}
+	
+	if(redundant){
+		for(n in dinucs){
+			dinucs.map{tetranucs.add(n + it)}
+		}
+	}else{
+		for(n in dinucs){
+			dinucs.map{
+				if(! tetranucs.contains(revComp(n + it))) tetranucs.add(n + it)}
+		}
 	}
+
 	return tetranucs.toList().sorted()
+}
+
+fun calcTetra(seq: String): Map<String,Int>{
+	val tetraList = makeTetraList()
+	val tMap =  tetraList.associate{ it to 0 }.toMutableMap<String,Int>()
+	val lastIdx = seq.length - 4
+	for(i in 0..lastIdx){
+		var piece = seq.substring(i,i+4)
+		if(piece in tetraList) tMap.put(piece, (tMap.get(piece) ?: 0) + 1)
+		piece = revComp(piece)
+		if(revComp(piece) in tetraList) tMap.put(piece, (tMap.get(piece) ?: 0) + 1)
+	}
+	return tMap
+}
+
+fun calcAllTetra(seq: String): Map<String,Int>{
+	val tMap = makeTetraList(true).associate{ it to 0 }.toMutableMap<String,Int>()
+	val lastIdx = seq.length - 4
+	for(i in 0..lastIdx){
+		val piece = seq.substring(i,i+4)
+		tMap.put(piece, (tMap.get(piece) ?: 0) + 1)
+	}
+	return tMap
 }
 
 fun main(args: Array<String>){
 	require(args.size>0) {"Provide file as a first argument"}
-	val allTetra = makeAllTetra()
 	val seqs = Fasta().read(args[0])
+	val redundant = false
+	val allTetra = makeTetraList(redundant)
+	if(redundant){
 	for (seq in seqs){
-		val tMap = mutableMapOf<String,Int>()
-		var i = 0
-		while(i+3 < seq.length){
-			val piece = seq.sequence.substring(i,i+4)
-			val count = tMap.getOrElse(piece,{0}) + 1
-			tMap.put(piece, count)
-			i++
-		}
+		val tMap = calcAllTetra(seq.sequence)
 		print(seq.id)
 		allTetra.forEach(){
-			print("\t${tMap.getOrElse(it,{0}).toDouble().div(seq.length-3)}")
+			print("\t${tMap.getOrElse(it,{0}).toDouble().times(100.0).div(seq.length-3)}")
 			// More time consumption if format() used
 			// print("\t%.5f".format(tMap.getOrElse(it,{0}).toDouble().div(seq.length-3)))
 		}
 		print("\n")
+	}
+	}else{
+	for (seq in seqs){
+		val tMap = calcTetra(seq.sequence)
+		print(seq.id)
+		allTetra.forEach(){
+			print("\t${tMap.getOrElse(it,{0}).toDouble().times(100.0).div(seq.length-3)}")
+			// More time consumption if format() used
+			// print("\t%.5f".format(tMap.getOrElse(it,{0}).toDouble().div(seq.length-3)))
+		}
+		print("\n")
+
+	}
 	}
 }
