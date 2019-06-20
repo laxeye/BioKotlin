2a9160d6f12a122a8dc2c9e4d6428a94781d4626
aleksei <oscypek@ya.ru>
2019-06-20 18:03:35 +0300

    A: NCBI BLAST parsing

--- README.md
+++ README.md
@@ -24,9 +24,18 @@ Run `gradle test` to perform few unit tests. Hopefully we'll have more in the fu
 
 ##Features
 
-- FASTA, FASTQ and gzipped FASTQ reading from files.
-- Getting any locus of sequence from FASTA file.
+- Reading FASTA (uncompressed) and FASTQ (uncompressed, gzip and bzip2) from files.
+- Getting any locus of sequence from FASTA file without prior indexing (reverse complement if strart > end).
 - Genome statistics from FASTA: N50, L50, N90, L90, GC(%), N's.
 - GFF parsing.
+- NCBI BLASTn results parsing for JSON (-outfmt 15).
 - Tetranucleotide composition statistics.
 - Removing orphan reads from paired-end FASTQ files (memory unefficient, but working).
+
+##ToDo
+- API description
+- DNA<->RNA, translation.
+- More tests.
+- NCBI BLASTp results parsing.
+- HMMER results parsing.
+- ...
---
+++ src/main/kotlin/ru/nrcki/bioKotlin/parsers/ncbiBlastJson.kt
@@ -0,0 +1,78 @@
+package space.akorzhenkov
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Optional
+import kotlinx.serialization.Serializable
+import java.io.*
+
+// Classes for serialization
+
+@Serializable
+data class BlastOut(val BlastOutput2: List<BlastResult>)
+
+@Serializable
+data class BlastResult(val report: BlastReport)
+
+@Serializable
+data class BlastReport(val program: String, val version: String,
+	val reference: String, val search_target: Target,
+	val params: Params, val results: Results)
+
+@Serializable
+data class Results(val bl2seq: List<Bl2seq>)
+
+@Serializable
+data class Bl2seq(val query_id: String,
+val query_title: String,
+val query_len: Int,
+@Optional val query_masking: List<QueryMasking>? = null,
+val hits: List<Hit>,
+val stat: Stat)
+
+@Serializable
+data class QueryMasking(val from: Int, val to: Int)
+
+@Serializable
+data class Target(val subjects: List<String>)
+
+@Serializable
+data class Params(val expect: Float,
+        val sc_match: Int,
+        val sc_mismatch: Int,
+        val gap_open: Int,
+        val gap_extend: Int,
+        val filter: String)
+
+@Serializable
+data class Description(val id: String, val title: String)
+
+@Serializable
+data class Hsp(val num: Int, val bit_score: Float,
+val score: Int, val evalue: Double,
+val identity: Int, val query_from: Int,
+val query_to: Int, val query_strand: String,
+val hit_from: Int, val hit_to: Int,
+val hit_strand: String, val align_len: Int,
+val gaps: Int, val qseq: String,
+val hseq: String, val midline: String)
+
+@Serializable
+data class Hit(val num: Int, val description: List<Description>, val len: Int,
+  val hsps: List<Hsp>)
+
+@Serializable
+data class Stat(val hsp_len: Int, 
+  val eff_space: Int, val kappa: Float,
+  val lambda: Float, val entropy: Float)
+
+//End
+
+fun parseNcbiBlastJson(filename: String): BlastOut{
+	val jsonString = File(filename).readLines().joinToString(" ")
+	val obj = Json(JsonConfiguration.Stable).parse(BlastOut.serializer(), jsonString)
+	return(obj)
+}
+
+fun blastOutput2AsListofReports(obj: BlastOut): List<BlastReport> = obj.BlastOutput2.map{it.report}
